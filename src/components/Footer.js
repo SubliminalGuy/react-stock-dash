895f866a5db64a308d5a1083243a2bde9249ce4f
Subliminal Guy <subliminal_kid@posteo.de>
2023-02-01 20:23:42 +0100

    Add Links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,25 @@ function Footer() {
   return (
     <div className="footer-container">
       <li className="footer-links">
-        <ul>Copyright</ul>
-        <ul>Mastodon</ul>
-        <ul>#50 React Projects</ul>
+        <ul>Copyleft</ul>
+        <ul>
+          <a
+            href="https://mastodon.sdf.org/@subliminal_guy"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mastodon
+          </a>
+        </ul>
+        <ul>
+          <a
+            href="https://50reactprojects.com"
+            target="_blank"
+            rel="noreferrer"
+          >
+            #50 React Projects
+          </a>
+        </ul>
       </li>
     </div>
   );
