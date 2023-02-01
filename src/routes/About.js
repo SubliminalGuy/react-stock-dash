fd70cfeea06ec8b72355af9621e3f456e4a55491
Subliminal Guy <subliminal_kid@posteo.de>
2023-02-01 20:19:27 +0100

    Add About Page

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,3 +1,27 @@
 export default function About() {
-  return <h1>Hello From About</h1>;
+  return (
+    <div className="about-container">
+      <p className="about-text">
+        I'm a media engineer from Germany. I currently live in Benalmádena on
+        the spanish Costa del Sol. I worked for every major tv-station in
+        Germany and i'm still remote working for two public broadcaster. So i
+        guess i'm somewhat a digital nomad ;-)
+      </p>
+      <p className="about-text">
+        My experiences include video production, content development, video
+        engineering and editing. 5 years ago i rediscovered my fascination for
+        programming and web development. I bootcamped in React, Vue and Svelte
+        and developed dozens of applications for training purposes.
+      </p>
+      <p className="about-text">
+        The <strong>Crypto Dashboard</strong> is part of my professional
+        portfolio. The prompt for this project came from Colby Fayock's
+        <a href="https://50reactprojects.com" target="_blank" rel="noreferrer">
+          #50 React Projects
+        </a>
+        and i will continue my journey with more projects from that resource. At
+        least until if find something better to do ...
+      </p>
+    </div>
+  );
 }
